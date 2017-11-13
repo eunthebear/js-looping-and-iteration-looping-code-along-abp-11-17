f850dc85b76df30f1de4a59a0ca4173666435b1c
eunthebear <eun.david@gmail.com>
2017-11-13 06:17:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function printBadges(employees) {
   }
   return employees;
 }
+
+function tailsNeverFails() {
+  let counter = 0;
+  while(Math.random() >= 0.5) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`;  
+}
